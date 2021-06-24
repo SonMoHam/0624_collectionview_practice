4fba095d3e30a5efcdabe1ea458e1697a6261c90
손모함 <73145656+SonMoHam@users.noreply.github.com>
2021-06-24 17:20:07 +0900

    Initial Commit

---
+++ 0624_collectionview_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0624_collectionview_practice
+//
+//  Created by 손대홍 on 2021/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
