df55a7d180201eb5926b4053f1d67a798a84081e
손모함 <73145656+SonMoHam@users.noreply.github.com>
2021-06-25 15:44:16 +0900

    ViewController swift file 추가

---
+++ 0624_collectionview_practice/MyCollectionVC.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionVC.swift
+//  0624_collectionview_practice
+//
+//  Created by 손대홍 on 2021/06/25.
+//
+
+import Foundation
+import UIKit
+
+class MyCollectionVC: UIViewController {
+    
+    //MARK: - Lifecycles
+}
