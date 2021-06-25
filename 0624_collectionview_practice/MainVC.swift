5e6585c6029a7fcf3e50e9c3ed0dd31da03a5241
손모함 <73145656+SonMoHam@users.noreply.github.com>
2021-06-25 15:49:28 +0900

    Main ViewController 스위프트 파일 추가

---
+++ 0624_collectionview_practice/MainVC.swift
@@ -0,0 +1,17 @@
+//
+//  MainVC.swift
+//  0624_collectionview_practice
+//
+//  Created by 손대홍 on 2021/06/25.
+//
+
+import Foundation
+import UIKit
+
+class MainVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("\(type(of: self)) - \(#function) called")
+    }
+}
